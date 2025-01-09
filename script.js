cf32abcf52cda51af9c5b028aa65cb7dedca594d
aboddrini <aboddrini99999@gmail.com>
2025-01-09 18:43:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+{
+    "name": "شركة خدمات تصميم وبرمجة",
+    "short_name": "خدمات تصميم",
+    "description": "موقع يقدم خدمات تصميم وبرمجة مميزة.",
+    "start_url": "/index.html",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#2c3e50",
+    "icons": [
+        {
+            "src": "icon.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        }
+    ]
+}
